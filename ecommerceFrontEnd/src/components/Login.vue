b912b7f6df9f8ed165cdd4b16cad560509b5fd53
NEUmmc <bmc.bob@qq.com>
2020-07-02 17:26:30 +0800

    登录和注册页面
    1.动画添加
    2.页面内数据逻辑的添加

--- ecommerceFrontEnd/src/components/Login.vue
+++ ecommerceFrontEnd/src/components/Login.vue
@@ -33,19 +33,33 @@
           show-password
         ></el-input>
         <el-button class="btn" type="danger" @click="login">Login</el-button>
-        <el-link class="register" type="danger" :underline="false" @click="change" icon="el-icon-right">register</el-link>
+        <el-link
+          class="register"
+          type="danger"
+          :underline="false"
+          @click="change"
+          icon="el-icon-right"
+        >register</el-link>
       </div>
     </transition>
 
     <transition name="el-zoom-in-left">
-      <div v-show="showRegister" class="box">
+      <div v-show="showRegister" class="box-register">
         <h2 class="title">Register</h2>
         <el-input
           style="width:24rem"
           class="input"
           v-model="username"
+          @input="usernameInput"
           placeholder="Please enter the user name."
           prefix-icon="el-icon-user-solid"
+        ></el-input>
+        <el-input
+          style="width:24rem"
+          class="input"
+          v-model="nickname"
+          placeholder="Please enter the nick name."
+          prefix-icon="el-icon-user"
         ></el-input>
         <el-input
           style="width:24rem"
@@ -55,8 +69,41 @@
           prefix-icon="el-icon-lock"
           show-password
         ></el-input>
-        <el-button class="btn" type="danger" @click="login">Register</el-button>
-        <el-link class="register" type="danger" :underline="false" @click="change" icon="el-icon-back">login</el-link>
+        <el-input
+          style="width:24rem"
+          class="input"
+          v-model="passwordSure"
+          @input="passwordSureInput"
+          placeholder="Please enter password again."
+          prefix-icon="el-icon-lock"
+          show-password
+        ></el-input>
+        <el-input
+          style="width:24rem"
+          class="input"
+          v-model="phone"
+          placeholder="Please enter your phone."
+          prefix-icon="el-icon-phone"
+        ></el-input>
+        <el-input
+          style="width:24rem"
+          class="input"
+          v-model="email"
+          placeholder="Please enter your e-mail."
+          prefix-icon="el-icon-message"
+        ></el-input>
+        <div class="radio">
+          <el-radio v-model="role_id" label="1">Brand-seller</el-radio>
+          <el-radio v-model="role_id" label="2">Borrow-seller</el-radio>
+        </div>
+        <el-button class="btn" type="danger" @click="register">Register</el-button>
+        <el-link
+          class="register"
+          type="danger"
+          :underline="false"
+          @click="change"
+          icon="el-icon-back"
+        >login</el-link>
       </div>
     </transition>
   </div>
@@ -69,31 +116,93 @@ export default {
       showlogin: true,
       showRegister: false,
       username: "",
-      password: ""
+      nickname: "",
+      password: "",
+      passwordSure: "",
+      phone: "",
+      email: "",
+      role_id: "1"
     };
   },
   methods: {
     change() {
       if (this.showlogin == true) {
         this.showlogin = false;
+        this.clearString()
         setTimeout(() => {
           this.showRegister = true;
         }, "350");
       } else {
         this.showRegister = false;
+        this.clearString()
         setTimeout(() => {
           this.showlogin = true;
         }, "350");
       }
     },
-    a() {
-      this.showRegister = true;
+    clearString(){
+        this.username = "";
+        this.password = "";
+        this.nickname = "";
+        this.passwordSure = "";
+        this.phone = "";
+        this.email = "";
+        this.role_id = "1";
+    },
+    usernameInput(){
+
+    },
+    passwordSureInput(){
+      if(this.password == this.passwordSure){
+        this.$notify({
+          title: 'Wonderfull',
+          message: 'Two passwords are the same',
+          position: 'bottom-right',
+          type:'success'
+        });
+      }else{
+        this.$notify({
+          title: 'Warning',
+          message: 'Two passwords are not the same',
+          position: 'bottom-right',
+          type:'warning'
+        });
+      }
+    },
+    register() {
+      if (this.username == "") {
+        this.$message.error("Please enter the username");
+        return;
+      }
+      if (this.nickname == "") {
+        this.$message.error("Please enter the nickname");
+        return;
+      }
+      if (this.Rpassword == "") {
+        this.$message.error("Please enter the password");
+        return;
+      }
+      if (this.passwordSure == "") {
+        this.$message.error("Please enter the password again");
+        return;
+      }
+      if (this.phone == "") {
+        this.$message.error("Please enter the phone");
+        return;
+      }
+      if (this.email == "") {
+        this.$message.error("Please enter the email");
+        return;
+      }
+      
+      this.change();
+      this.$message.success("Registered successfully, now jump to login");
     },
     login() {
       if (this.username == "") {
-        this.$message.error("请输入用户名~");
+        this.$message.error("请输入用户名");
       } else if (this.password == "") {
-        this.$message.error("请输入密码～");
+        this.$message.error("请输入密码");
       } else {
         //无后端演示登录
         if (this.username == "admin" && this.password == "admin888") {
@@ -138,11 +247,26 @@ export default {
   justify-content: space-evenly;
   height: 25rem;
 }
-.title{
+.box-register {
+  margin-top: 5rem;
+  width: 85%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 40rem;
+}
+.title {
   width: 35rem;
 }
 .input >>> .el-input__inner {
   height: 50px !important;
+}
+.radio >>> .el-radio__input.is-checked .el-radio__inner {
+  border-color: #f46774;
+  background: #f46774;
+}
+.radio >>> .el-radio__input.is-checked + .el-radio__label {
+  color: #f46774;
 }
 .btn {
   width: 15rem;
