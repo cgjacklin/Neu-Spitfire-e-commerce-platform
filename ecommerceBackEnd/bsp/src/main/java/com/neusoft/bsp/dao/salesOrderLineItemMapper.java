0be3ee8d18dc99e08e09114715787234cc5882ba
TQSsssss <1192185532@qq.com>
2020-07-05 20:45:20 +0800

    change the generated mappers

--- ecommerceBackEnd/bsp/src/main/java/com/neusoft/bsp/dao/salesOrderLineItemMapper.java
+++
@@ -1,9 +0,0 @@
-package com.neusoft.bsp.dao;
-
-import com.neusoft.bsp.model.salesOrderLineItem;
-
-public interface salesOrderLineItemMapper {
-    int insert(salesOrderLineItem record);
-
-    int insertSelective(salesOrderLineItem record);
-}
