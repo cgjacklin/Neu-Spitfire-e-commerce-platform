4c8ba7328483593fb3eac7e49c0d684d79cc93d7
Canis <1025221390@qq.com>
2020-07-12 13:52:29 +0800

    disable redis

--- ecommerceBackEnd/bsp/src/main/java/com/neusoft/bsp/BspApplication.java
+++ ecommerceBackEnd/bsp/src/main/java/com/neusoft/bsp/BspApplication.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @MapperScan("com.neusoft.bsp.*.*.mapper")
 @SpringBootApplication
 @EnableSwagger2
-@EnableCaching
+//@EnableCaching
 public class BspApplication {
 
     public static void main(String[] args) {
