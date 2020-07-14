b79ea027300096dfbd5624510e70580a93bd65af
NEUmmc <bmc.bob@qq.com>
2020-07-14 16:17:26 +0800

    解决心愿单bug

--- ecommerceFrontEnd/src/components/page/BVO/GoodsList.vue
+++ ecommerceFrontEnd/src/components/page/BVO/GoodsList.vue
@@ -137,37 +137,36 @@ export default {
       search_name: "",
       type: "",
       goods: [],
-      wishlist:[]
+      wishlist: []
     };
   },
-  mounted(){
+  mounted() {
     this.$post("/product/getProductOnShelf", {
       user_id: sessionStorage.getItem("user_id")
     }).then(res => {
-      if(res.code == 504){
+      if (res.code == 504) {
         this.$notify.warning(res.message);
         return;
       }
-      if(res.code == 200){
+      if (res.code == 200) {
         this.goods = res.data;
-        console.log(res)
+        console.log(res);
       }
-    })
+    });
   },
   methods: {
-    refresh(){
+    refresh() {
       this.$post("/product/getProductOnShelf", {
-      user_id: sessionStorage.getItem("user_id")
+        user_id: sessionStorage.getItem("user_id")
       }).then(res => {
-        if(res.code == 504){
+        if (res.code == 504) {
           this.$notify.warning(res.message);
           return;
         }
-        if(res.code == 200){
+        if (res.code == 200) {
           this.goods = res.data;
-          
         }
-      })
+      });
     },
     EhandleCheckAllChange(val) {
       this.checkedEStores = val ? this.Estores : [];
@@ -194,65 +193,60 @@ export default {
       this.$notify.success("Successfull");
     },
     //添加到心愿单，记得调refresh()刷新一下
-    star(item) {
-         this.$post("/wit/getWishlist", {
+    async getWishList() {
+      await this.$post("/wit/getWishlist", {
         user_id: sessionStorage.getItem("user_id")
       }).then(res => {
         let temp = [];
-        for (var i = 0; i <  res.data.number; i++) { 
-      temp.push(
-                {
-          name: res.data.product[i].title,
-          src: res.data.product[i].remark,
-          price: res.data.product[i].retail_price,
-          brand:res.data.brand[i].name_en,
-          stock:res.data.product[i].replenishment_period,
-          amazondescription:res.data.packageinfo[i].amazon_description,
-          ebaydescription:res.data.packageinfo[i].ebay_description,
-          witid:res.data.wishlist[i].wit_id,
-          star: 1,
-          proid:res.data.product[i].pro_id
+        for (var i = 0; i < res.data.number; i++) {
+          temp.push({
+            name: res.data.product[i].title,
+            src: res.data.product[i].remark,
+            price: res.data.product[i].retail_price,
+            brand: res.data.brand[i].name_en,
+            stock: res.data.product[i].replenishment_period,
+            amazondescription: res.data.packageinfo[i].amazon_description,
+            ebaydescription: res.data.packageinfo[i].ebay_description,
+            witid: res.data.wishlist[i].wit_id,
+            star: 1,
+            proid: res.data.product[i].pro_id
+          });
         }
-                )
- } 
         this.wishlist = temp;
-      })
-      var witid;
-      for(var i=0;i<this.wishlist.length;i++){
-          if(item.pro_id == this.wishlist[i].proid){
+      });
+    },
+    star(item) {
+      this.getWishList().then(r => {
+        var witid;
+        for (var i = 0; i < this.wishlist.length; i++) {
+          if (item.pro_id == this.wishlist[i].proid) {
             witid = this.wishlist[i].witid;
           }
-      }
-    if(typeof (witid) != 'undefined' | item.star==2){
-  if (item.star == 1) {
-        this.$post("/wit/deletedWishlist", {
-        wit_id: witid
-      }).then(res => {
-        item.star = 2;
-        this.refresh
-      })
-        return;
-      }else{
-       this.$post("/wit/addWishlist", {
-        user_id:sessionStorage.getItem("user_id"),
-        pro_id:item.pro_id,
-      }).then(res => {
-        item.star = 1;       
-        this.refresh
-      })
-      }
-
-    }
-    
+        }
+        if (item.star == 1) {
+          this.$post("/wit/deletedWishlist", {
+            wit_id: witid
+          }).then(res => {
+            item.star = 2;
+            this.refresh;
+          });
+          return;
+        } else {
+          this.$post("/wit/addWishlist", {
+            user_id: sessionStorage.getItem("user_id"),
+            pro_id: item.pro_id
+          }).then(res => {
+            item.star = 1;
+            this.refresh;
+          });
+        }
+      });
     },
     detail(item) {
-   
       this.chooseItem = item;
       this.drawer = true;
     },
-    search() {
-   
-    }
+    search() {}
   }
 };
 </script>
