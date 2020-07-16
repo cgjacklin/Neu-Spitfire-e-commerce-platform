7203789d69a692edd8da2581be6518246251da28
Megumin <36215541+1209920141@users.noreply.github.com>
2020-07-16 20:57:05 +0800

    Permission management v1.0.

---
+++ ecommerceBackEnd/bsp/src/main/java/com/neusoft/bsp/admin/menu/vo/MenuDetailWithID.java
@@ -0,0 +1,40 @@
+package com.neusoft.bsp.admin.menu.vo;
+
+public class MenuDetailWithID {
+    int menu_id;
+    String menu_name;
+    String menu_url;
+    String menu_icon;
+
+    public int getMenu_id() {
+        return menu_id;
+    }
+
+    public void setMenu_id(int menu_id) {
+        this.menu_id = menu_id;
+    }
+
+    public String getMenu_name() {
+        return menu_name;
+    }
+
+    public void setMenu_name(String menu_name) {
+        this.menu_name = menu_name;
+    }
+
+    public String getMenu_url() {
+        return menu_url;
+    }
+
+    public void setMenu_url(String menu_url) {
+        this.menu_url = menu_url;
+    }
+
+    public String getMenu_icon() {
+        return menu_icon;
+    }
+
+    public void setMenu_icon(String menu_icon) {
+        this.menu_icon = menu_icon;
+    }
+}
